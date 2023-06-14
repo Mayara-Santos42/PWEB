d2542887b19938442eb13812acef19f5df156676
Mayara Santos <98860380+Mayara-Santos42@users.noreply.github.com>
2023-06-13 23:36:04 -0300

    Delete Atividade 16 directory

--- Atividade 16/ex05.js
+++
@@ -1,7 +0,0 @@
-var eventos = require('events');
-var EmissorEventos = eventos.EventEmitter;
-var ee = new EmissorEventos();
-
-ee.on('dados', fecha => console.log(fecha));
-
-setInterval(() => ee.emit('dados', Date.now()), 500);
--- Atividade 16/ex06.js
+++
@@ -1,3 +0,0 @@
-var http = require('http');
-var server = http.createServer((req, res) => res.end('<html><body>Site da Fatec Sorocaba</body></html>'));
-server.listen(3000);
